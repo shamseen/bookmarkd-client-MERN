ace9f6f3ffc6afcca10bc3b18142b95ce432eda3
shamseen <shamseen.c@gmail.com>
2021-05-02 19:09:48 -0400

    index.js rendering works.. not app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./styles/App.scss";
+
+export default function App() {
+    console.log("app.js");
+    return (
+        <div className="App">
+            {/* boilerplate */}
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("JS linked")
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+    <React.StrictMode>
+        <div style={{ color: "white" }}>wassup</div>
+    </React.StrictMode >,
+    rootElement
+);
